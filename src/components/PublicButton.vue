4287335ca3e35d1b13d8399d79cf18debf8c963f
Emma <1285641941@qq.com>
2022-02-18 22:24:02 +0800

    feat: public button component

---
+++ src/components/PublicButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="button">
+    <slot>一个按钮</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  color?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  color: "#5379FF99",
+});
+</script>
+
+<style lang="scss">
+.button {
+  width: 228rpx;
+  height: 44px;
+  line-height: 44px;
+  border: 2rpx solid #ffffff;
+  border-radius: 10px;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    #dfc1fd 0%,
+    rgba(223, 193, 253, 0) 100%
+  );
+  font-family: OPPOSans;
+  font-size: 18px;
+  color: v-bind("props.color");
+}
+</style>
