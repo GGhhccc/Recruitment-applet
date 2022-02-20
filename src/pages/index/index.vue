d5a9340b6d19d525ab1b1d5862405ee201b8797d
lgh <2524398864@qq.com>
2022-02-20 22:35:58 +0800

    bug: ts声明问题

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,6 @@
 <template>
-  <view class="safe-area">
-    <GroupToIntroduction :cardName="cardName" />
-  </view>
+  <view class="safe-area"> </view>
 </template>
-<script setup lang="ts">
-import { ref } from "vue";
-import GroupToIntroduction from "../../components/GroupToIntroduce";
-const cardName = ref("backEnd"); //"Android"、"deepLearning”、"UI"、"backEnd"、"lab"、"frontEnd";输入不同名称切换组件
-</script>
+<script setup lang="ts"></script>
 
 <style></style>
