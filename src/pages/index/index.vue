7809601318d565e7b43ade720d58ce1998f46aa8
olddog <3056085618@qq.com>
2022-02-26 16:44:13 +0800

    <feature>(<scoped>):<完成了表单报名的样式及功能>

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <view class="safe-area"> </view>
+  <view class="safe-area">
+    <Form :disabled="true"></Form>
+  </view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Form from "../../components/Form.vue";
+</script>
 
 <style></style>
